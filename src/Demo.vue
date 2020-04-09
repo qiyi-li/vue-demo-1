79770034f5f05e5663714ab17bfb4801b335f985
a-lei-7 <qiyi.li@aol.com>
2020-04-09 21:22:33 +0800

    update

---
+++ src/Demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="color">
+    Demo的内部
+    {{message}}
+    <button @click="fn">call fn</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['message','fn']
+  }
+</script>
+
+<style >
+  .color{
+    color: cornflowerblue;
+    border: 1px solid cadetblue;
+  }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+const Vue =window.Vue
 Vue.config.productionTip = false
 
+import Demo from './Demo.vue'
+
+// eslint-disable-next-line no-unused-vars
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  components:{Demo},
+  data :{
+    visible: true,
+    n: 0
+  }
+  ,
+  template:`
+      <div>
+          {{n}}
+          <Demo :message="n" :fn="add"/>
+      </div>
+  `,
+  methods:{
+    add(){
+      this.n +=1
+    },
+    toggle() {
+      this.visible = !this.visible
+    }
+  }
+}).$mount("#ergou")
